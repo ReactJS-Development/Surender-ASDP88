fe95810af8099819881a2412d2345c62e633625c
surenderhsr44 <suenderhsr44@gmail.com>
2018-09-08 13:33:59 +0530

    this app statless and state full

---
+++ src/Statelesscomponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Statelesscomponent = ({startWidth,name}) => {
+	return(
+		<div>
+		this value is stateless components:{startWidth} 
+		<p>name is:{name}</p>
+		</div>
+		);
+}
+export default  Statelesscomponent;
